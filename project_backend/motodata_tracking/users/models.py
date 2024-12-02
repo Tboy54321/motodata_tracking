cc515428ef4763063a0a9acc1596b2cdd0f5b3a7
tboy54321 <akinniranoluwatosin22@gmail.com>
2024-12-02 23:31:58 +0100

    Updated

--- project_backend/motodata_tracking/users/models.py
+++ project_backend/motodata_tracking/users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    is_service_adviser = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
+    
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='customer_profile')
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.IntegerField(max_length=20)
+    address = models.TextField(max_length=100)
+    # profile_picture = 
+
+    def __str__(self):
+        return self.first_name
+    
+class ServiceAdviserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='service_adviser_profile')
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    department = models.CharField(max_length=15)
+    phone_number = models.CharField(max_length=20)
+    # profile_picture
+
+    def __str__(self):
+        return self.first_name
+    
--- project_backend/motodata_tracking/vehicles/urls.py
+++ project_backend/motodata_tracking/vehicles/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.userDashboard, name='dashboard'),
+    path('dashboard/', views.userDashboard, name='vehicle-dashboard'),
     path('vehicle-page/', views.userVehiclesPage, name='vehicles-page'),
     path('vehicle-details/', views.userVehiclesDetails, name='vehicles-details'),
     path('sa/vehicle-management/', views.saVehiclesManagement, name='vehicles-management'),
-    path('sa/', views.saDashboard, name='sa-dashboard'),
+    path('sa/dashboard/', views.saDashboard, name='sa-dashboard'),
 ]
